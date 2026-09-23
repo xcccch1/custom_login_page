e8d4726d5292baffd8b6581c859635980b19fe6a
xcccch1 <xcccch1@163.com>
2026-09-23 11:20:33 +0800

    feat: odoo18版本

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Custom Database Display Name',
+    'author': 'xcccch1',
+    'version': '18.0.1.0.0',
+    'license': 'LGPL-3',
+    'category': 'Operations/Custom Frontend',
+    'summary': 'Show mapped display names for databases on login screens',
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'views/webclient_templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
